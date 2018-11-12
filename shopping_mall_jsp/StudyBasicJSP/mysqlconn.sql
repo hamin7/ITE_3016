9e39a4d65050bde72931aa0546e2874cc6af996e
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-11-12 21:12:34 +0900

    Update mysqlconn.sql

--- shopping_mall_jsp/StudyBasicJSP/mysqlconn.sql
+++ shopping_mall_jsp/StudyBasicJSP/mysqlconn.sql
@@ -1,49 +1,82 @@
 show databases;
 
+drop table member;
+
 create table member(
 id varchar(50) not null primary key,
 passwd varchar(16) not null,
 name varchar(10) not null,
-reg_date datetime not null,
-address varchar(100) not null,
-tel varchar(20) not null
+reg_date datetime not null
 );
 
-create table juventus(
-num varchar(5) not null primary key,
-nation varchar(10) not null,
-position varchar(5) not null,
-name varchar(50) not null,
-birthday datetime not null,
-height varchar(10) not null,
-weight varchar(10) not null
+create table test(
+num_id int not null primary key auto_increment,
+title varchar(50) not null,
+content text not null
 );
-
-
-insert into member(id, passwd, name, reg_date, address, tel)
-values('Federico@gmail.com','0233','Federico',now(),'Corso Gaetano Scirea, 50, Turin, Italy','010-2482-8229');
-
-insert into member(id, passwd, name, reg_date, address, tel)
-values('Mattia@gmail.com','0513','Mattia',now(),'Via Piccolomini 5, 20151, Milan, Italy','010-0513-2011');
-
-update member set tel = '010-0233-2018' where id='Federico@gmail.com';
-
-desc member;
-desc juventus;
-
-select * from member;
-select * from member where id='Federico@gmail.com';
-select * from juventus;
-
-select id, name
-from member;
-
-delete from member where id='Mattia@gmail.com';
-delete from member;
-
-drop table member;
 
 show tables;
+drop table test;
 
-insert into juventus(num, nation, position, name, birthday, height, weight)
-values('001','Poland','GK','Wojciech Szczęsny','1990.04.18','195cm','90kg');
+insert into member(id, passwd, name, reg_date)
+values('Federico@gmail.com','1234', 'Federico',now());
+
+insert into member(id, passwd, name, reg_date)
+values('Mattia@gmail.com','1111','Mattia',now());
+
+select * from member;
+
+select id, passwd from member;
+
+select id, passwd from member where id='hongkd@aaa.com';
+
+update member set passwd='3579' where id='hongkd@aaa.com';
+
+select * from member where id='hongkd@aaa.com';
+
+delete from member where id='hongkd@aaa.com';
+
+delete from member;
+
+create table board(
+num int not null primary key auto_increment,
+writer varchar(10) not null,
+email varchar(30),
+subject varchar(50) not null,
+passwd varchar(12) not null,
+reg_date datetime not null,
+readcount int default 0,
+ref int not null,
+re_step smallint not null,
+re_level smallint not null,
+content text not null,
+ip varchar(20) not null
+);
+
+alter table member
+add address varchar(100) not null;
+
+alter table member
+add tel varchar(20) not null;
+
+update member
+set address = 'Holloway, London, N5, England', tel='010-1886-0601'
+where id='Federico@gmail.com';
+
+update member
+set address='Corso Gaetano Scirea, 50, Turin, Italy', tel='010-2482-8229'
+where id='Mattia@gmail.com';
+
+
+insert into member(id, passwd, name, reg_date, address, tel)
+values('Gonzalo@gmail.com', '1109', 'Gonzalo', now(), 'Via Piccolomini 5, 20151, Milan, Italy', '010-0513-2011');
+
+create table manager(
+managerId varchar(50) not null primary key,
+managerPasswd varchar(16) not null
+);
+
+insert into manager(managerid, managerPasswd)
+values('bookmaster@shop.com', '123456');
+
+
