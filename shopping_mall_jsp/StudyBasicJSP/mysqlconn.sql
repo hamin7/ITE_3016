0d1071145e98827c1dee89506c9d9d4e38336b9d
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-11-13 22:04:52 +0900

    Update mysqlconn.sql

--- shopping_mall_jsp/StudyBasicJSP/mysqlconn.sql
+++ shopping_mall_jsp/StudyBasicJSP/mysqlconn.sql
@@ -6,7 +6,9 @@ create table member(
 id varchar(50) not null primary key,
 passwd varchar(16) not null,
 name varchar(10) not null,
-reg_date datetime not null
+reg_date datetime not null,
+address varchar(100) not null,
+tel varchar(20) not null
 );
 
 create table test(
@@ -67,6 +69,12 @@ update member
 set address='Corso Gaetano Scirea, 50, Turin, Italy', tel='010-2482-8229'
 where id='Mattia@gmail.com';
 
+insert into member(id, passwd, name, reg_date, address, tel)
+values('Federico@gmail.com','1234', 'Federico',now(), 'Holloway, London, N5, England', '010-1886-0601');
+
+insert into member(id, passwd, name, reg_date, address, tel)
+values('Mattia@gmail.com','1111','Mattia',now(),'Corso Gaetano Scirea, 50, Turin, Italy', tel='010-2482-8229');
+
 
 insert into member(id, passwd, name, reg_date, address, tel)
 values('Gonzalo@gmail.com', '1109', 'Gonzalo', now(), 'Via Piccolomini 5, 20151, Milan, Italy', '010-0513-2011');
@@ -79,4 +87,60 @@ managerPasswd varchar(16) not null
 insert into manager(managerid, managerPasswd)
 values('bookmaster@shop.com', '123456');
 
+create table book(
+book_id int not null primary key auto_increment,
+book_kind varchar(3) not null,
+book_title varchar(100) not null,
+book_price int not null,
+book_count smallint not null,
+author varchar(40) not null,
+publishing_com varchar(30) not null,
+publishing_date varchar(15) not null,
+book_image varchar(16) default'nothing.jpg',
+book_content text not null,
+discount_rate tinyint default 10,
+reg_date datetime not null
+);
 
+desc book;
+
+create table bank(
+account varchar(30) not null,
+bank varchar(10) not null,
+name varchar(10) not null
+);
+
+insert into bank(account, bank, name)
+values('11111-111-11111', 'Santander', 'Fernando');
+
+select * from bank;
+
+create table cart(
+cart_id int not null primary key auto_increment,
+buyer varchar(50) not null,
+book_id int not null,
+book_title varchar(100) not null,
+buy_price int not null,
+buy_count tinyint not null,
+book_image varchar(16) default 'nothing.jpg'
+);
+
+desc cart;
+
+create table buy(
+buy_id bigint not null,
+buyer varchar(50) not null,
+book_id varchar(12) not null,
+book_title varchar(100) not null,
+buy_price int not null,
+buy_count tinyint not null,
+book_image varchar(16) default 'nothing.jpg',
+buy_date datetime not null,
+account varchar(50) not null,
+deliveryName varchar(10) not null,
+deliveryTel varchar(20) not null,
+deliveryAddress varchar(20) not null,
+sanction varchar(10) default '상품 준비'
+);
+
+desc buy;
